dafd7d8cde3056a03f93214a345c3c2bfcb34fe0
barnali12 <barnali@customercaresolutions.com>
2019-08-09 13:19:11 -0700

    extracted scripts from profiles

--- tosca/database/scripts.go
+++ tosca/database/scripts.go
@@ -1,17 +1,22 @@
 package database
 
 import (
+	//"fmt"
+	"github.com/tliron/puccini/ard"
+	"github.com/tliron/puccini/tosca/problems"
 	"strings"
 
 	"github.com/tliron/puccini/tosca"
 	"github.com/tliron/puccini/tosca/parser"
+	"github.com/tliron/puccini/url"
 )
 
-func createScriptNamespace(grammerVersions string) tosca.ScriptNamespace {
-
+func CreateScriptNamespace(grammerVersions string, internalImport string) tosca.ScriptNamespace {
 	versions := strings.Split(grammerVersions, ",")
 
-	toscaContext := tosca.NewContext(nil, nil)
+	problems := new(problems.Problems)
+
+	toscaContext := tosca.NewContext(problems, nil)
 
 	for _, ver := range versions {
 
@@ -19,6 +24,23 @@ func createScriptNamespace(grammerVersions string) tosca.ScriptNamespace {
 		reader := grammar["MergeScriptNamespace"]
 
 		reader(toscaContext)
+		paths := make([]string, 0)
+
+		toscaPath := parser.ProfileInternalPaths[ver]
+		//kubePath := "/tosca/kubernetes/1.0/profile.yaml"
+		paths = append(paths, toscaPath)
+		paths = append(paths, internalImport)
+
+		for _, path := range paths {
+			if profileURL, err := url.NewValidInternalURL(path); err == nil {
+				data, _ := ard.ReadURL(profileURL)
+				toscaContext.URL = profileURL
+				toscaContext.Data = data["metadata"]
+				reader := grammar["Metadata"]
+				reader(toscaContext)
+			}
+
+		}
 
 	}
 
